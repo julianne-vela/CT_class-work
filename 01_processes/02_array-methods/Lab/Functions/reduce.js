7fcca8a4069e1a11b11bf6f6a6e648cd5846e8b5
Julianne Vela <75726146+julianne-vela@users.noreply.github.com>
2021-03-23 19:34:47 -0700

    reduce test PASSED! was off by 2 initially because acc was 0 instead of array[0](which was 2)

--- 01_processes/02_array-methods/Lab/Functions/reduce.js
+++ 01_processes/02_array-methods/Lab/Functions/reduce.js
@@ -1,16 +1,18 @@
-const reduce = (array, callback, acc = 0) => {
-    for (let i = 0; i < array.length; i++) {
-        if (callback(acc, i)) {
-            let acc = array[1];
-
+const reduce = (array, callback, initialValue) => {
+    // IV starts as ACC or 0 if no IV given
+    let acc = initialValue || array[0];
+    // if no IV, ACC is index0 and item is index1
+    if (!initialValue) {
+        for (let i = 1; i < array.length; i++) {
+            acc = callback(acc, array[i], i);
         }
-        else {
-            let acc = i;
-
-        }
-
-        
     }
+    else {
+        for (let i = 0; i < array.length; i++) {
+            acc = callback(acc, array[i], i)
+        }
+    }
+    return acc;
 };
 
 module.exports = reduce;
--- 01_processes/02_array-methods/Lab/__tests__/reduce.test.js
+++ 01_processes/02_array-methods/Lab/__tests__/reduce.test.js
@@ -1,10 +1,10 @@
 const reduce = require("../Functions/reduce");
 
 describe('reduce function', () => {
-    it('should add each callback return value to the next function call and return the final acc total.', () => {
+    it('should return the final accumulator total of all callbacks given an array and callback fn.', () => {
         const array = [2, 3, 5, 6, 4, 1, 3, 0, 9];
         const callback = (acc, n) => acc + n;
 
-        expect(reduce(array, callback)).toEqual(28);
+        expect(reduce(array, callback)).toEqual(33);
     });
 });
