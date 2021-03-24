2e002b175ad135cae7c9d9a3244a03b9d102ee7a
Julianne Vela <75726146+julianne-vela@users.noreply.github.com>
2021-03-23 18:40:38 -0700

    additional test for every function to verify false result when any callback is not true

--- 01_processes/02_array-methods/Lab/Functions/every.js
+++ 01_processes/02_array-methods/Lab/Functions/every.js
@@ -1,8 +1,12 @@
 const every = (array, callback) => {
     for (let i = 0; i < array.length; i++) {
-        if (callback(array[i], i)) return true;        
+        if (callback(array[i], i)) {
+            return true;
+        }
+        else if (!callback(array[i], i)) {
+            return false;
+        }
     }
-    return false;
 };
 
 module.exports = every;
